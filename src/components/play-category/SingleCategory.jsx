5aa3f3d960f0d25eaa56e88feee319ce5a7d4b4f
teeliny <teeliny@gmail.com>
2021-12-17 21:26:02 +0100

    added mock data for single category

--- src/components/play-category/SingleCategory.jsx
+++ src/components/play-category/SingleCategory.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
 import Pagination from '../../common/pagination/Pagination';
+import featured from '../../mockData/featured-playlist.json';
 
 function SingleCategory() {
+  const tracks = featured.tracks.data;
+  const arr = tracks.map(item => (
+    {
+      id: item.id,
+      title: item.title,
+      preview: item.preview,
+      img: item.album.cover,
+      small_img: item.album.cover_small,
+    }
+  ));
+  console.log(arr)
   return (
     <SingleWrapper>
       <div className='category__header'>
@@ -10,8 +22,47 @@ function SingleCategory() {
         <hr className='category__line' />
         <Pagination />
       </div>
-      <div>
-        content
+      <div className='category__content'>
+        {/* Single content */}
+        <div className='main__content'>
+          <img className='content__img' src={arr[0].img} alt={arr[0].title} />
+          <p className='content__text'>{arr[0].title}</p>
+        </div>
+        {/* Single content */}
+        <div className='main__content'>
+          <img className='content__img' src={arr[0].img} alt={arr[0].title} />
+          <p className='content__text'>{arr[0].title}</p>
+        </div>
+        {/* Single content */}
+        <div className='main__content'>
+          <img className='content__img' src={arr[0].img} alt={arr[0].title} />
+          <p className='content__text'>{arr[0].title}</p>
+        </div>
+        {/* Single content */}
+        <div className='main__content'>
+          <img className='content__img' src={arr[0].img} alt={arr[0].title} />
+          <p className='content__text'>{arr[0].title}</p>
+        </div>
+        {/* Single content */}
+        <div className='main__content'>
+          <img className='content__img' src={arr[0].img} alt={arr[0].title} />
+          <p className='content__text'>{arr[0].title}</p>
+        </div>
+        {/* Single content */}
+        <div className='main__content'>
+          <img className='content__img' src={arr[0].img} alt={arr[0].title} />
+          <p className='content__text'>{arr[0].title}</p>
+        </div>
+        {/* Single content */}
+        <div className='main__content'>
+          <img className='content__img' src={arr[0].img} alt={arr[0].title} />
+          <p className='content__text'>{arr[0].title}</p>
+        </div>
+        {/* Single content */}
+        <div className='main__content'>
+          <img className='content__img' src={arr[0].img} alt={arr[0].title} />
+          <p className='content__text'>{arr[0].title}</p>
+        </div>
       </div>
       
     </SingleWrapper>
@@ -43,6 +94,26 @@ const SingleWrapper = styled.div`
     width: 10%;
     transform: translateY(-50%);
     border: 1px solid #eeeeee;
+  }
+
+  .category__content {
+    display: flex;
+    justify-content: space-between;
+    padding: 0 1.5rem;
+    gap: 0.5rem;
+  }
+
+  .main__content {
+    /* width: 70%; */
+    margin: 0 auto;
+  }
+
+  .content__img {
+    width: 200px;
+  }
+
+  .content__text {
+    text-align: center;
   }
 
   @media(min-width: 601px) {
