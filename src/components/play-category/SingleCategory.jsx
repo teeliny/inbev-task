33609ee4dc3cc4f5ccbafe5b616e7108561fd8b9
teeliny <teeliny@gmail.com>
2021-12-21 14:00:15 +0100

    show message only on hover

--- src/components/play-category/SingleCategory.jsx
+++ src/components/play-category/SingleCategory.jsx
@@ -7,6 +7,7 @@ function SingleCategory({data, category}) {
   const { layout: pageSize, handleClickTrack } = useContext(ScreenContext);
   const [displayData, setDisplayData] = useState([]);
   const [pageIndex, setPageIndex] = useState(1);
+  const [isHover, setIsHover] = useState(false);
 
   // Extract the data to display based on page index
   useEffect(() => {
@@ -31,6 +32,8 @@ function SingleCategory({data, category}) {
           <div 
             key={singleTrack.id} 
             id={singleTrack.id}
+            onMouseEnter={() => setIsHover(true)}
+            onMouseLeave={() => setIsHover(false)}
             onClick={() => handleClickTrack(singleTrack.id)}
             className='main__content'
           >
@@ -40,7 +43,7 @@ function SingleCategory({data, category}) {
               alt={singleTrack.title} 
             />
             <p className='content__text'>{singleTrack.title}</p>
-            {singleTrack.preview.length === 0 &&
+            {isHover && singleTrack.preview.length === 0 &&
               <p className='track__error'>Audio file not provided</p>
             }
           </div>
